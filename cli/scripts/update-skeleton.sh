c6de39975ec1eab26ee9bfd29584aeac58edf19e
Martin Kopeček <martin.kopecek@smallhill.cz>
2025-10-16 14:17:07 +0200

    fix: use mkdir -p to create target directory

--- cli/scripts/update-skeleton.sh
+++ cli/scripts/update-skeleton.sh
@@ -28,7 +28,7 @@ if git clone --quiet -b $SKELETON_BRANCH --depth=1 $TARGET_ORIGIN target; then
     cd target
 else
     echo -e "Branch \033[33m$SKELETON_BRANCH\033[0m not found in \033[33m$TARGET_ORIGIN\033[0m, initializing..."
-    mkdir target
+    mkdir -p target
     cd target
     git init -b $SKELETON_BRANCH
     git commit -m "${COMMIT_PREFIX}init" --allow-empty
